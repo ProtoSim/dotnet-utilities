3bbcd6915f7800d4572367246cd6b30780e5b13b
Sebastian Romanet <sebastian@protosim.net>
2021-04-25 21:32:32 -0700

    added Strings class with IsValidGuid check

---
+++ Strings.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ProtoSim.Utilities {
+    public static class Strings {
+        public static bool IsValidGuid(this string guid) {
+            if (string.IsNullOrEmpty(guid))
+                return false;
+
+            return Regex.IsMatch(guid, @"\w{8}-(\w{4}-){3}\w{12}");
+        }
+    }
+}
