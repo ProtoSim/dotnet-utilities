7293b04f50d82aa28099f0c25232bf132c00bdbe
Sebastian Romanet <sebastian@protosim.net>
2021-04-25 23:49:07 -0700

    added comments to Strings.IsValidGuid method

--- Strings.cs
+++ Strings.cs
@@ -2,6 +2,12 @@
 
 namespace ProtoSim.DotNetUtilities {
     public static class Strings {
+        /// <summary>
+        /// Checks the given string to determine if it is a valid Guid value
+        /// </summary>
+        /// <remarks>Null string objects will return False</remarks>
+        /// <param name="guid">The string value to check</param>
+        /// <returns>returns True if valid or False if invalid</returns>
         public static bool IsValidGuid(this string guid) {
             if (string.IsNullOrEmpty(guid))
                 return false;
