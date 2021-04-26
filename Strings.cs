f5616b07c4f280887d95c6f4d429a1a532290128
Sebastian Romanet <sebastian@protosim.net>
2021-04-26 00:17:22 -0700

    added Strings.IsValidEmailAddress method

--- Strings.cs
+++ Strings.cs
@@ -3,9 +3,22 @@
 namespace ProtoSim.DotNetUtilities {
     public static class Strings {
         /// <summary>
+        /// Checks the given string to determine if it is a valid email address value
+        /// </summary>
+        /// <remarks>A NULL string object will return False</remarks>
+        /// <param name="emailAddress">The string value to check</param>
+        /// <returns>returns True if valid or False if invalid</returns>
+        public static bool IsValidEmailAddress(this string emailAddress) {
+            if (string.IsNullOrEmpty(emailAddress))
+                return false;
+
+            return Regex.IsMatch(emailAddress, @"^[a-z0-9]+([-_.]{1}[a-z0-9]+)*@[a-z0-9]+([-]{1}[a-z0-9]+)*[.][a-z]{2,}");
+        }
+
+        /// <summary>
         /// Checks the given string to determine if it is a valid Guid value
         /// </summary>
-        /// <remarks>Null string objects will return False</remarks>
+        /// <remarks>A NULL string object will return False</remarks>
         /// <param name="guid">The string value to check</param>
         /// <returns>returns True if valid or False if invalid</returns>
         public static bool IsValidGuid(this string guid) {
