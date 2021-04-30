1bb2441e5c48a77f8e9f715f624029db5e29d3c7
Sebastian Romanet <sebastian@protosim.net>
2021-04-30 10:14:15 -0700

    fixed summary documentation for Strings class methods

--- Strings.cs
+++ Strings.cs
@@ -10,9 +10,8 @@ namespace ProtoSim.DotNetUtilities {
         /// <summary>
         /// Checks if <paramref name="emailAddress"/> is a valid email address value
         /// </summary>
-        /// <remarks>A NULL string object will return False</remarks>
-        /// <param name="emailAddress">The string value to check</param>
-        /// <returns>True if valid or False if invalid</returns>
+        /// <param name="emailAddress">The <c>string</c> value to check</param>
+        /// <returns><c>true</c> if valid or <c>false</c> if invalid</returns>
         public static bool IsValidEmailAddress(this string emailAddress) {
             if (string.IsNullOrEmpty(emailAddress))
                 return false;
@@ -23,9 +22,8 @@ namespace ProtoSim.DotNetUtilities {
         /// <summary>
         /// Checks if <paramref name="guid"/> is a valid Guid value
         /// </summary>
-        /// <remarks>A NULL string object will return False</remarks>
-        /// <param name="guid">The string value to check</param>
-        /// <returns>True if valid or False if invalid</returns>
+        /// <param name="guid">The <c>string</c> value to check</param>
+        /// <returns><c>true</c> if valid or <c>false</c> if invalid</returns>
         public static bool IsValidGuid(this string guid) {
             if (string.IsNullOrEmpty(guid))
                 return false;
@@ -34,12 +32,12 @@ namespace ProtoSim.DotNetUtilities {
         }
 
         /// <summary>
-        /// Converts the given <paramref name="item"/> to a plain string, taking into account its Type
+        /// Converts the given <paramref name="item"/> to a plain string, taking into account its <c>Type</c>
         /// </summary>
-        /// <remarks>Will return the standard object.ToString() result if unsupported Type</remarks>
+        /// <remarks>Will return the standard <c>object.ToString()</c> result if unsupported Type</remarks>
         /// <param name="item">The item to convert</param>
         /// <param name="parenthesesInsteadOfCarets">Use parentheses instead of carets for applicable Types</param>
-        /// <returns>A string object representing <paramref name="item"/></returns>
+        /// <returns>A <c>string</c> object representing <paramref name="item"/></returns>
         public static string ToPlainString(this object item, bool parenthesesInsteadOfCarets = false) {
             if (item == null)
                 return null;
