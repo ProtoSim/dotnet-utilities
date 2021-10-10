e08645836454f430135c26f2368c4b3e5b158fb0
Sebastian Romanet <sebastian@protosim.net>
2021-10-09 19:42:27 -0700

    added a Timer extension class and a Restart method

---
+++ Timer.cs
@@ -0,0 +1,18 @@
+﻿namespace ProtoSim.DotNetUtilities {
+    /// <summary>
+    /// Provides extension methods for the <see cref="System.Timers.Timer"/> Type, adding functionality.
+    /// </summary>
+    public static class Timer {
+        /// <summary>
+        /// Restarts the given <paramref name="timer"/>.
+        /// </summary>
+        /// <remarks>Calls the <c>Stop()</c> and <c>Start()</c> methods on the <see cref="System.Timers.Timer"/>.</remarks>
+        public static void Restart(this System.Timers.Timer? timer) {
+            if (timer == null)
+                return;
+
+            timer.Stop();
+            timer.Start();
+        }
+    }
+}
